e247cc2ab8e7ab66419c560d8d935545b0e2dc89
Kevin <magavakevin10@gmail.com>
2022-07-19 19:19:52 -0400

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (event) => {
   // your code here
+  const htmlform = document.querySelector('form');
+  htmlform.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    newTask = document.getElementById('new-task-description').value;
+    AddtoList(newTask);
+  })
+  //for practice JS implementation lines 9 - 27
+  /* //creates label and select tag
+  let selecttag = document.createElement('select');
+  let labeltag = document.createElement('label');
+  labeltag.textContent = 'Priority';
+
+  //appends the labeltag and selecttag after form elements
+  htmlform.after(labeltag, selecttag);
+
+  //array of options
+  const optionlist = ['High', 'Medium', 'Low'];
+
+  //for each option, we create an option tag, give it the text and value of the item in array
+  optionlist.forEach((item) => {
+    let optiontag = document.createElement('option');
+    optiontag.value = item;
+    optiontag.textContent = item;
+    let select = document.querySelector('select')
+    select.appendChild(optiontag);
+  }) */
+  //
 });
+
+
+const AddtoList = (newTask) => {
+  let p = document.createElement('p');
+  let delbtn = document.createElement('button');
+  delbtn.textContent = 'X';
+  delbtn.addEventListener("click", DeleteItem);
+  p.textContent = `${newTask}`;
+  p.appendChild(delbtn);
+  p.style.backgroundColor = colorvalue[colorkey];
+  document.getElementById('tasks').appendChild(p);
+}
+
+const DeleteItem = (e) => {
+  e.target.parentNode.remove();
+} 
+
+const colorvalue={
+  '':'white',
+  'High':'red',
+  'Medium':'yellow',
+  'Low':'green',
+};
+
+
+let colorkey;
+
+//event listening for selection change
+document.getElementById('task-color').addEventListener('change', () => colorkey = document.getElementById('task-color').value);
+
+
+
+
+
+
