c768acc9c0ab5efc1eadb674773484a9478b3306
Kevin <magavakevin10@gmail.com>
2022-07-22 18:14:36 -0400

    Updated for sorting

--- updatedindex.js
+++ updatedindex.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
     
     //get html elements
     const htmlform = document.querySelector('form');
-    const ultask = document.getElementById('tasks');
+    
 
     //create dropdown
     htmlform.appendChild(divtag)
@@ -28,13 +28,13 @@ document.addEventListener('DOMContentLoaded', (e) => {
     for (const key in Priority){
         let optiontag = document.createElement('option')
         optiontag.innerText = key;
-        optiontag.value = Priority[key]['color'];
+        optiontag.value = Priority[key];
         selecttag.appendChild(optiontag)
     }
 
     //add sortbutton
     htmlform.appendChild(sortbutton)
-    sortbutton.addEventListener('click', SortColors(e))
+    sortbutton.addEventListener('click', SortColors)
 
     //form eventlistener
     htmlform.addEventListener('submit', (e) => {
@@ -44,56 +44,90 @@ document.addEventListener('DOMContentLoaded', (e) => {
         
         //takes description to be added to list
         const newTask = document.getElementById('new-task-description').value;
-    
         //prevent empty entries
         if (newTask !== ''){
             AddtoList(e, newTask)
         }
     })
 
+
+    const AddtoList = (e, newTask) => {
+
+        let ultask = document.getElementById('tasks');
+
+        //assign select priority 
+        let prio = e.target.dropdown.value;
+
+        //create li with task description
+        let li = document.createElement('li');
+        li.innerText = newTask;
+        li.style.backgroundColor = prio;
+        li.classList.add(position(prio))
+
+
+        //create del button w/ eventlistener
+        let btn = document.createElement('button')
+        btn.textContent = ' X ';
+        // 
+
+        //attach button to child
+        li.appendChild(btn)
+        btn.addEventListener('click', Deletion)
+        ultask.appendChild(li)
+    
+        htmlform.reset()
+    }
+
 })
 
-const AddtoList = (e, newTask) => {
-    //assign select priority 
-    let prio = e.target.dropdown.value
-
-    //create li with task description
-    let li = document.createElement('li')
-    li.innerText = newTask
-    li.style.backgroundColor = prio
-
-    //create del button w/ eventlistener
-    let btn = document.createElement('button')
-    btn.textContent = ' X '
-    btn.addEventListener ('click', Deletion(e))
-
-    //attach button to child
-    li.appendChild(btn)
-
-    ultask.appendChild(li)
-
-    form.reset()
-}
 
 const Deletion = (e) => {
     e.target.parentNode.remove();
 }
 
-//global object container for assignment of color and sorting value
-const Priority = {
-    High:{
-        color: 'red',
-        value: 1,
-    },
-    Medium:{
-        color: 'yellow',
-        value: 2,
-    },
-    Low:{
-        color: 'green',
-        value: 3
-    }
+
+const position = (prio) => {
+    const priovalarr = Object.values(Priority)
+    return priovalarr.indexOf(prio)
 }
 
-//array for sorting
-const sortArray = []
+const SortColors = () => {
+    //grab the elements we'll be modifying
+    const ul_task = document.getElementById('tasks')
+
+    //getelementbyID and QuerySelector => return return the node element
+    //getelemetnsbytagname, class, or queryslectorall
+    const nodearr = document.querySelectorAll('li')
+
+    //turn the array
+    let workingArr = Array.from(nodearr)
+    workingArr = workingArr.sort((a,b) => {
+        //parseInt Classname from string into number
+        let c = parseInt(a.className) 
+        let d = parseInt(b.className)
+        console.log(a, b)
+        if (c < d){
+            return -1
+          } else if (c > d){
+            return 1
+          } else {
+            return 0
+          }
+        }
+        )
+    while (ul_task.firstChild){
+        ul_task.removeChild(ul_task.firstChild);
+        }
+    workingArr.forEach((element) => {ul_task.appendChild(element)})
+
+}   
+
+//global object container for assignment of color and sorting value
+const Priority = {
+    High: 'red',
+    Medium:'yellow',
+    Low: 'green',
+}
+
+
+
